b61ab4e36b84f719d6035b0b0bd81e6e931c2e0e
duch <top@secret.org>
2018-06-30 21:50:00 +0200

    config file

---
+++ Config.py
@@ -0,0 +1,25 @@
+Config = {
+    # database
+    'db_name': 'words',
+    'db_host': 'localhost',
+    'db_user': 'root',
+    'db_pass': '',
+    'queue_rows_per_sec': 10,
+    'queue_sleep_seconds': 5,
+
+    # crawler
+    'common_words_num': 100,
+
+    # Wit.ai
+    'wit_access_token': '5K3A4ZNU5ZAYP6DTWO3GS7KQSFAAQCPU',
+
+    # hashing
+    'hashing_salt': '04SbMmtWik^YdECP1mdM18z3y#kl!yGq',
+
+    # encryption
+    'private_key_path': 'private.pem',
+    'public_key_path': 'public.pem',
+
+    # front-end
+    'frontend_dir': 'frontend'
+}
